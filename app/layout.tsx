8e6c4a9c2290a5b193bfc176d4a54c1fee14b2f5
Ferrow <gerhat.f@gmail.com>
2024-05-24 17:07:02 +0200

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/google-analytics";
 import Navigation from "@/components/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -26,6 +27,9 @@ export default function RootLayout({
         ></script>
       </head>
       <body className={`${inter.className}`}>
+        {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS ? (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+        ) : null}
         <Navigation />
         {children}
       </body>
